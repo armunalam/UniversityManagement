1e27b1d3593f2f029038e890965106b1c0be3b39
Armun Alam <armunalam@gmail.com>
2021-07-14 15:28:37 +0600

    Progress 3

--- university_management/models/classroom.py
+++ university_management/models/classroom.py
@@ -1,8 +1,10 @@
 from odoo import fields, models, api
+from odoo.exceptions import ValidationError
 
 
 class Classroom(models.Model):
     _name = 'uni.classroom'
+    _rec_name = 'course'
 
     department = fields.Many2one('uni.department', string='Department', required=True)
     course = fields.Many2one('uni.course', string='Course', required=True)
@@ -37,3 +39,34 @@ class Classroom(models.Model):
                 'course': [('department', '=', self.department.id)],
             }
         }
+
+    @api.constrains('time_from')
+    def constraint_from(self):
+        query_string = '''
+            SELECT cl.id
+            FROM uni_classroom cl
+            WHERE (((cl.time_from <= {} AND cl.time_to >= {})
+               OR (cl.time_from < {} AND cl.time_to > {}))
+               OR (cl.time_from >= {} AND cl.time_to <= {}))
+              AND cl.room_no = '{}'
+              AND cl.day = '{}'
+              AND cl.create_date != '{}'
+              AND cl.write_date != '{}';
+        '''.format(self.time_from, self.time_from,
+                   self.time_to, self.time_to,
+                   self.time_from, self.time_to,
+                   self.room_no, self.day,
+                   self.create_date, self.write_date)
+
+        self._cr.execute(query_string)
+
+        print(query_string)
+        fetch_data = self._cr.fetchone()
+        print(fetch_data)
+        if fetch_data:
+            print('Found')
+            raise ValidationError('The time and room overlap with another course.')
+        else:
+            print('Not found')
+
+
